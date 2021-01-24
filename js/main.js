d912c449076048f1e5d7695c031f1d6b45f93456
Billy Cook <cook0318@algonquinlive.com>
2021-01-24 17:33:24 -0500

    Added HTML, input logic, styling

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
-var Keys = new KeyGenerator();
-
-function EncryptText (text, key) {
-    let encryptedText = "";
-    for (i = 0; i < text.length; i++) {
-        let char = text.charAt(i);
-        let numericRepresentation = ConvertCharacterToNumber(char);
-        let bigInt = EncryptNumber(numericRepresentation, key);
-        let paddedHexValue = ConvertBigIntToHex(bigInt);
-        encryptedText += paddedHexValue;
-    }
-
-    return encryptedText;
+function GenerateKeys() {
+    var keys = new KeyGenerator();
+    ShowKeyDataInTable(keys);
 }
 
-function DecryptText (encryptedText, key) {
-    let decryptedText = "";
-    let hexSlices = SplitHexString(encryptedText);
-    hexSlices.forEach(hex => {
-        let encryptedNumber = ConvertHexToInt(hex);
-        let decryptedNumber = DecryptNumber(encryptedNumber, key);
-        let decryptedLetter = ConvertNumberToCharacter(decryptedNumber);
-        decryptedText += decryptedLetter;
-    });
+function ShowKeyDataInTable(keys) {
+    $('#P').html(keys.P);
+    $('#Q').html(keys.Q);
+    $('#N').html(keys.N);
+    $('#Phi').html(keys.Phi);
+    $('#FactorsN').html(keys.FactorsOfN.join());
+    $('#FactorsPhi').html(keys.FactorsOfPhi.join());
+    $('#E').html(keys.EncryptionNumber);
+    $('#D').html(keys.DecryptionNumber);
 
-    return decryptedText;
+    $('#PiKey').html(keys.PrivateKey.join());
+    $('#PuKey').html(keys.PublicKey.join());
 }
 
+function EncryptUserInput(){
+    let input = $('#rawInput').val();
+    let d = $('#privateKeyD').val();
+    let n = $('#privateKeyN').val();
+    let result = $('#encryptedResult');
+    let key = [d,n];
+
+    let encryptedText = EncryptText(input, key);
+    result.val(encryptedText);
+}
+
+function DecryptUserInput(){
+    let input = $('#encryptedInput').val();
+    let e = $('#publicKeyE').val();
+    let n = $('#publicKeyN').val();
+    let result = $('#decryptedResult');
+    let key = [e,n];
+
+    let decryptedText = DecryptText(input, key);
+    result.val(decryptedText);
+}
+
+function ClearAllInputs(){
+    $('#rawInput').val("");
+    $('#privateKeyD').val("");
+    $('#privateKeyN').val("");
+    $('#encryptedResult').val("");
+    $('#encryptedInput').val("");
+    $('#publicKeyE').val("");
+    $('#publicKeyN').val("");
+    $('#decryptedResult').val("");
+    $('#copyResult').val("");
+}
--- js/utilities/CryptographyService.js
+++ js/utilities/CryptographyService.js
@@ -1,14 +1,40 @@
-function EncryptNumber(num, publicKey) {
-    let d = publicKey[0];
-    let n = publicKey[1];
+function EncryptText (text, key) {
+    let encryptedText = "";
+    for (i = 0; i < text.length; i++) {
+        let char = text.charAt(i);
+        let numericRepresentation = ConvertCharacterToNumber(char);
+        let bigInt = EncryptNumber(numericRepresentation, key);
+        let paddedHexValue = ConvertBigIntToHex(bigInt);
+        encryptedText += paddedHexValue;
+    }
+
+    return encryptedText;
+}
+
+function DecryptText (encryptedText, key) {
+    let decryptedText = "";
+    let hexSlices = SplitHexString(encryptedText);
+    hexSlices.forEach(hex => {
+        let encryptedNumber = ConvertHexToInt(hex);
+        let decryptedNumber = DecryptNumber(encryptedNumber, key);
+        let decryptedLetter = ConvertNumberToCharacter(decryptedNumber);
+        decryptedText += decryptedLetter;
+    });
+
+    return decryptedText;
+}
+
+function EncryptNumber(num, key) {
+    let d = key[0];
+    let n = key[1];
     let encryptedNumber = fastModularExponentiation(BigInt(num), BigInt(d), BigInt(n));
 
     return encryptedNumber;
 }
 
-function DecryptNumber(encryptedNumber, privateKey) {
-    let e = privateKey[0];
-    let n = privateKey[1];
+function DecryptNumber(encryptedNumber, key) {
+    let e = key[0];
+    let n = key[1];
     let decryptedNumber = fastModularExponentiation(BigInt(encryptedNumber), BigInt(e), BigInt(n));
 
     return parseInt(decryptedNumber);
